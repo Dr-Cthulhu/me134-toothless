ba5e985a8c6d8f447fb12e0a1daf899c2b17138c
Emily Pan <espan@caltech.edu>
2019-03-05 01:23:44 -0800

    halfway through dice node stuff

--- src/myrobot/src/dice_recognition.cpp
+++ src/myrobot/src/dice_recognition.cpp
@@ -8,153 +8,126 @@
  */
 
  // std lib
- #include <iostream>
- #include <sstream>
+#include <iostream>
+#include <sstream>
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 using namespace std;
 using namespace cv;
 #include "opencv2/opencv.hpp"
- // OpenCV
- #include <opencv2/core.hpp>
- #include "opencv2/objdetect.hpp"
- #include <opencv2/imgproc.hpp>
- //#include <opencv2/highgui.hpp>
- #include <opencv2/features2d.hpp>
+// OpenCV
+#include <opencv2/core.hpp>
+#include "opencv2/objdetect.hpp"
+#include <opencv2/imgproc.hpp>
+//#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+// ROS + CVBridge
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+
+static const std::string OPENCV_WINDOW = "Image window";
 
 class DiceDetector {
-	// Constraints
 
-public:
 	ros::NodeHandle nh_;
+  	image_transport::ImageTransport it_;
+ 	image_transport::Subscriber image_sub_;
+ 	image_transport::Publisher image_pub_;
 
-	boost::mutex connect_mutex_;
-	bool use_rbgd_;
+ public:
+ 	DiceDetector()
+  		: it_(nh_)
+ 	{
+ 		// Subscribe to input video feed and publish pointcloud
+  		image_sub_ = it_.subscribe("/camera/image_raw", 1, &DiceDetector::findDice, this);
+		point_pub_ = nh_.advertise<sensor_msgs::PointCloud>("dice_points", 1);
+	}
 
-	// Subscription topics
-	string image_image_;
+	~DiceDetector() {
+	}
 
-	// RBG-D
-	string camera_;
-	string camera_topic_;
-	string depth_image_;
-	string depth_topic_;
-	string camera_info_topic;
-	string depth_info_topic;
-	string rbg_ns_;
-	string depth_ns_;
-
-	image 
-
-
-	int countPips(cv::Mat dice){
-
-	  // resize
+	int countPips(cv::Mat dice) {
+	  	// resize
 		cv::resize(dice, dice, cv::Size(150, 150));
 
-	  // convert to grayscale
-	  cvtColor(dice, dice, CV_BGR2GRAY);
+	  	// convert to grayscale
+	  	cvtColor(dice, dice, CV_BGR2GRAY);
 
-	  // threshold
-	  cv::threshold(dice, dice, 150, 255, cv::THRESH_BINARY | CV_THRESH_OTSU );
+	  	// threshold
+	  	cv::threshold(dice, dice, 150, 255, cv::THRESH_BINARY | CV_THRESH_OTSU );
 
-	  // show
-	  cv::namedWindow("processed", true);
-	  cv::imshow("processed", dice);
-
-
-	  // floodfill
-	  cv::floodFill(dice, cv::Point(0,0), cv::Scalar(255));
-	  cv::floodFill(dice, cv::Point(0,149), cv::Scalar(255));
-	  cv::floodFill(dice, cv::Point(149,0), cv::Scalar(255));
-	  cv::floodFill(dice, cv::Point(149,149), cv::Scalar(255));
-
-	  // search for blobs
-	  cv::SimpleBlobDetector::Params params;
-
-	  // filter by interia defines how elongated a shape is.
-	  params.filterByInertia = true;
-	  params.minInertiaRatio = 0.5;
-
-	  // will hold our keyponts
-	  std::vector<cv::KeyPoint> keypoints;
-
-	  // create new blob detector with our parameters
-	  cv::Ptr<cv::SimpleBlobDetector> blobDetector = cv::SimpleBlobDetector::create(params);
-
-	  // detect blobs
-	  blobDetector->detect(dice, keypoints);
+	  	// show
+	  	cv::namedWindow("processed", true);
+	  	cv::imshow("processed", dice);
 
 
-	  // return number of pips
-	  return keypoints.size();
+	  	// floodfill
+		cv::floodFill(dice, cv::Point(0,0), cv::Scalar(255));
+		cv::floodFill(dice, cv::Point(0,149), cv::Scalar(255));
+		cv::floodFill(dice, cv::Point(149,0), cv::Scalar(255));
+		cv::floodFill(dice, cv::Point(149,149), cv::Scalar(255));
+
+		// search for blobs
+		cv::SimpleBlobDetector::Params params;
+
+		// filter by interia defines how elongated a shape is.
+		params.filterByInertia = true;
+		params.minInertiaRatio = 0.5;
+
+		// will hold our keyponts
+		std::vector<cv::KeyPoint> keypoints;
+
+		// create new blob detector with our parameters
+		cv::Ptr<cv::SimpleBlobDetector> blobDetector = cv::SimpleBlobDetector::create(params);
+
+		// detect blobs
+		blobDetector->detect(dice, keypoints);
+
+		// return number of pips
+		return keypoints.size();
 	}
 
 	bool isSquare(vector<Point> contour) {
-	  int peri = cv::arcLength(contour, true);
-	  vector<Point> approx;
-	  cv::approxPolyDP(contour, approx, 0.04 * peri, true);
-	  if (approx.size() == 4) {
-	    cv::RotatedRect bound = cv::minAreaRect(contour);
-	    int ar = bound.size.width / bound.size.height;
-	    if (ar > 0.9 && ar < 1.1)
-	      return true;
-	  }
-	  return false;
+	  	int peri = cv::arcLength(contour, true);
+	  	vector<Point> approx;
+	  	cv::approxPolyDP(contour, approx, 0.04 * peri, true);
+	  	if (approx.size() == 4) {
+		    cv::RotatedRect bound = cv::minAreaRect(contour);
+		    int ar = bound.size.width / bound.size.height;
+		    if (ar > 0.9 && ar < 1.1)
+		      	return true;
+	  	}
+	  	return false;
 	}
 
-	int main( int argc, char** argv )
-	{
+	void findDice(const sensor_msgs::ImageConstPtr& msg) {
 
-	  cout <<"start";
-	  // open window frame
-	  cv::namedWindow("frame", true);
+		// Initialize point-cloud
+		sensor_msgs::PointCloud all;
+		ros::Time scan_time = ros::Time::now();
+		all.header.frame_id = "world_frame";
 
-	  while(true){
-	    // open the default camera
-	    cv::VideoCapture cap(0);
-	    
+		cv_bridge::CvImagePtr cv_ptr;
+		try {
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& e) {
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
 
-	    // check if we succesfully opened the camera stream
-	    if(!cap.isOpened()){
-	      cout <<"coouldnt open camera \n";
-	      return -1;
-	    }
-
-	    // set camera properties
-	    cap.set(CV_CAP_PROP_FRAME_WIDTH,1920);
-	    cap.set(CV_CAP_PROP_FRAME_HEIGHT,1080);
-
-	    //  take single frame (without dices) and convert it to grayscale
-	    //  to use when removing the background
-	    cv::Mat backgroundFrame;
-	    cap >> backgroundFrame;
-	    cvtColor(backgroundFrame, backgroundFrame, CV_BGR2GRAY);
-
-	    // will hold our frame
-	    //cv::Mat frame;
-	    //cv::Mat unprocessFrame;
 	    Mat frame;
-	    Mat unprocessFrame;
 	    int totalDotCount = 0;
 	    Scalar markingColor = Scalar(0, 200, 0);
 
-	    cap >> frame;
-	    //cap.read(frame);
-
+	    frame = cv_ptr->image;
 	    
-	    unprocessFrame = frame.clone();
-	    cv::imshow("frame", unprocessFrame);
-	    waitKey(33);
-	    // if (waitKey(30) >= 0)
-	    // {
-	    //   continue;
-	    //   }
-
 	    inRange(unprocessFrame, Scalar(200, 200, 200), Scalar(255, 255, 255), frame);
 	    //double diceContourArea = cv::contourArea(diceContours[0]);
 	    Mat element = getStructuringElement(MORPH_RECT, Size(3, 3), Point(1, 1));
@@ -166,33 +139,40 @@ public:
 	    findContours(frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
 	    int numberOfDice = 0;
-	    int dotCount = 0;
-	    int last_square_index = 0;
+	    vector<Point> real;
 
 	    for(int i = 0; i < contours.size(); i++){
-	      if (hierarchy[i][3] == -1) {
-	        if (!isSquare(contours[i]))
-	          continue;
-	        numberOfDice += 1;
-	        dotCount = 0;
-	        last_square_index = i;
-	      } else {
-	        dotCount += 1;
-
-	        if ((i == contours.size() - 1) || (hierarchy[i+1][3] == -1)) {
-	          totalDotCount += dotCount;
-
-	          stringstream stream;
-	          stream << dotCount;
-	          Moments m = moments(contours[last_square_index]);
-	          putText(unprocessFrame, stream.str(), Point(m.m10 / m.m00 + 100, m.m01 / m.m00), FONT_HERSHEY_PLAIN, 2, markingColor, 2);
-	        }
-	      }
-	      drawContours(unprocessFrame, contours, i, markingColor, 2, 8, hierarchy, 0, Point());
+	      	if (hierarchy[i][3] == -1) {
+		        if (!isSquare(contours[i]))
+		          	continue;
+		        numberOfDice += 1;
+		        for (int j = 0; j < contours[i].size(); j++) {
+		        	Point realPoint;
+		        	cv::RotatedRect bound = cv::minAreaRect(contours[i]);
+		        	realPoint.x = bound.x + bound.size.width / 2;
+		        	realPoint.y = bound.y + bound.size.width / 2;
+		        }
+		        real.append(realPoint);
+		    }
 	    }
-	    
-	    cv::imshow("frame", unprocessFrame);
-	    waitKey(33);
+
+	    all.points.resize(numberOfDice);
+	    for (int i = 0; i < numberOfDice; i++) {
+	    	sensor_msgs::Point32 point;
+	    	point.x = real[i].x;
+	    	point.y = real[i].y;
+	    	all.points[i] = point;
+	    }
+	    point_pub_.publish(all);
 	}
-	
 }
+
+int main( int argc, char** argv )
+{
+	ros::init(argc, argv, "dice_publisher");
+	DiceDetector dd;
+	ros::spin();
+	return 0;
+}
+	
+
